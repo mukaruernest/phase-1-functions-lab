a5fd932822c2c4ff0b528369dfe61781ef5bfa28
mukaruernest <mukaruernesto@gmail.com>
2023-01-11 16:18:01 +0300

    Functions Lab Submission

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const hq = 42
+const distanceFromHqInBlocks = function (pickup) {
+    const distance = hq - pickup
+    return Math.abs(distance)
+}
+
+function distanceFromHqInFeet(value) {
+    const inFeet = distanceFromHqInBlocks(value) * 264;
+    return inFeet
+}
+
+// function distanceTravelledInFeet()
+function distanceTravelledInFeet(start, destination) {
+    const distance = Math.abs(start - destination)
+    const toFeet = distance * 264
+    return toFeet
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    } else if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        const fourTwothousand = distanceTravelledInFeet(start, destination) - 400
+        return fourTwothousand * 0.02
+    } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+        return 25
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far'
+    }
+}
